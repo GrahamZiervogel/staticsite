ea4a660ca1f58116524a60e190bc0b9c9241ef34
GrahamZiervogel <grahamziervogel@gmail.com>
2025-02-16 16:27:45 +0000

    Setup test framework

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
-from textnode import TextNode
-from textnode import TextType
+from textnode import TextNode, TextType
 
 
 def main():
     text = "This is a text node"
-    text_type = TextType("bold")
+    text_type = TextType.BOLD
     url = "https://www.boot.dev"
     text_node = TextNode(text, text_type, url)
     print(text_node)
---
+++ src/test_textnode.py
@@ -0,0 +1,33 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_text_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node ", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_text_type_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.ITALIC)
+        self.assertNotEqual(node, node2)
+
+    def test_url_not_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD, "www.google.com")
+        node2 = TextNode("This is a text node", TextType.BOLD, "www.yahoo.com")
+        self.assertNotEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("test text", TextType.ITALIC, "google.com")
+        self.assertEqual(repr(node), "TextNode(test text, italic, google.com)")
+
+
+if __name__ == "__main__":
+    unittest.main()
